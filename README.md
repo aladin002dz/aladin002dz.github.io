d2225c8863c63c0d4c1625b5e7261df24c8127a4
aladin002dz <mahfoudh.arous@gmail.com>
2022-02-08 15:39:00 +0100

    toggle dark mode

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Welcome to my personal website code.  
 visit it [here](https://aladin002dz.github.io)
+
+This website was build by [custimzing bootstrap](https://getbootstrap.com/docs/5.1/customize/sass/).
---
+++ js/index.js
@@ -0,0 +1,46 @@
+const chk = document.getElementById("toggle-mode");
+const aboutCard = document.getElementById("about-card");
+const socialIcons = document.getElementById("social-icons");
+
+let darkMode = localStorage.getItem("darkMode") || "false";
+if (darkMode === "true") {
+  chk.checked = true;
+  addDark();
+}
+
+document.body.classList.remove("d-none");
+
+chk.addEventListener("change", toggleMode);
+
+function toggleMode() {
+  if (chk.checked === true) {
+    addDark();
+    localStorage.setItem("darkMode", "true");
+    darkMode = "true";
+  } else {
+    removeDark();
+    localStorage.setItem("darkMode", "false");
+  }
+}
+
+function addDark() {
+  document.body.classList.remove("bg-light");
+  document.body.classList.add("dark");
+
+  aboutCard.classList.remove("bg-white");
+  aboutCard.classList.add("bg-dark");
+
+  socialIcons.classList.remove("text-dark");
+  socialIcons.classList.add("text-light");
+}
+
+function removeDark() {
+  document.body.classList.add("bg-light");
+  document.body.classList.remove("dark");
+
+  aboutCard.classList.add("bg-white");
+  aboutCard.classList.remove("bg-dark");
+
+  socialIcons.classList.add("text-dark");
+  socialIcons.classList.remove("text-light");
+}
