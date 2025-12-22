c2a4a131c564fcd16a462b495e7fb806378d3733
aladin002dz <mahfoudh.arous@gmail.com>
2025-12-22 11:34:04 +0100

    ✨ feat: Add GitHub Pages deployment workflow and configure Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
